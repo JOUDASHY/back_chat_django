31f265c6433e92a1862c7df06685d4c7a326652a
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-05-10 22:51:58 +0300

    fix static

--- config/settings.py
+++ config/settings.py
@@ -167,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line to define where static files will be collected
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
