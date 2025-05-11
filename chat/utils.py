2554c8b43fe86c9ce2aafc52b9aa84786c49bb60
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-05-11 20:52:01 +0300

    h

--- chat/utils.py
+++ chat/utils.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 # Tentative de connexion à Redis, avec gestion d'erreur
 try:
-    redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+    redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
     redis_available = True
 except redis.exceptions.ConnectionError:
     redis_client = None
