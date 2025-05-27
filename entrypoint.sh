e958353b5a6170df01ba480037ca1aebd54608a6
JOUDASHY <alitsiryeddynilsen@gmail.com>
2025-05-27 12:43:17 +0300

    aa

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,3 +16,4 @@ echo "✅ MySQL is up - launching Django..."
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+ 
